2d805044298f6b80d1c3bd6b0e5ce41e3d9bcb98
Alina Galimova <Tyuiu.galimovaas@outlook.com>
2024-10-03 23:09:49 +0500

    Task7

--- Tyuiu.GalimovaAS.Sprint1.Task7.V14.Lib/DataService.cs
+++ Tyuiu.GalimovaAS.Sprint1.Task7.V14.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.GalimovaAS.Sprint1.Task7.V14.Lib
     {
         public double Calculate(double x, double y)
         {
-            double res = Math.Round(Math.Pow(2, -x) + (5 * Math.Pow(x, 2)) / (3 * Math.Pow(x, 2)) - Math.Cos(Math.Pow(x, 2)) + Math.Sin(2 * x * y), 3);
+            double res = Math.Round(Math.Pow(2, -x) + (5 * Math.Pow(x, 2)) / (3 * Math.Pow(x, 3)) - Math.Cos(Math.Pow(x, 2)) + Math.Sin(2 * x * y), 3);
             return res;
         }
     }
